90ad605a4dac652b70911b0f81282d1c3cb16b91
neal <neal2034@gmail.com>
2021-05-31 22:14:14 +0800

    auth 基本配置完成

--- src/renderer/pages/api/sub-components/api-config-area.tsx
+++ src/renderer/pages/api/sub-components/api-config-area.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Tabs} from "antd";
 import ConfigParams from "./config-params";
 import {API} from "../apiSlice";
+import ConfigAuth from "./config-auth";
 
 const {TabPane} = Tabs
 interface ApiProps{
@@ -16,7 +17,9 @@ export default function ApiConfigArea(props:ApiProps){
             <TabPane tab="Params" key="1">
                 <ConfigParams api={props.api}/>
             </TabPane>
-            <TabPane tab="Authorization" key="2">here is Authorization</TabPane>
+            <TabPane tab="Authorization" key="2">
+                <ConfigAuth api={props.api}/>
+            </TabPane>
             <TabPane tab="Headers" key="3">here is Headers</TabPane>
             <TabPane tab="Body" key="4">here is Body</TabPane>
             <TabPane tab="Tests" key="5">here is Tests</TabPane>
---
+++ src/renderer/pages/api/sub-components/config-auth.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {API} from "../apiSlice";
+import {Select, Input} from "antd";
+import './api-wrapper.less'
+
+const {Option} = Select
+
+interface ApiProps{
+    api: API
+}
+
+
+export default function ConfigAuth(props:ApiProps){
+
+    return (
+        <div className="d-flex justify-between config-auth">
+        <div className="d-flex align-center">
+            <span  className="mr10">鉴权类型</span>
+            <Select defaultValue="INHERIT" style={{ width: 200 }}>
+                <Option value="INHERIT">继承</Option>
+                <Option value="NONE">无</Option>
+                <Option value="BEARER">Bearer Token</Option>
+            </Select>
+        </div>
+
+            <div className="d-flex align-center">
+                <span className="mr10">Token</span>
+                <Input></Input>
+            </div>
+    </div>
+    )
+}
