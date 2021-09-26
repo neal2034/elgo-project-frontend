b2b025bc23af92623b0db8e38770b679b16dd61e
neal <neal2034@gmail.com>
2021-09-26 20:13:58 +0800

    mac and web use same code to build

--- src/renderer/Pages.tsx
+++ src/renderer/Pages.tsx
@@ -15,10 +15,14 @@ export default function Pages(){
     const {enqueueSnackbar, closeSnackbar} = useSnackbar()
     effToast.setSnackBar(enqueueSnackbar,closeSnackbar)
 
+    const isElectron = window && window.process && window.process.type == 'renderer'
+    const homePath = isElectron ? '/login':'/home'
+
+
   return ( <Router>
         <Switch>
-            {/*<Route exact path="/" render={() => <Redirect to='/login' push/>}/>*/}
-            <Route exact path="/" component={HomePage} />
+            <Route exact path="/" render={() => <Redirect to={homePath} push/>}/>
+            <Route path="/home" component={HomePage} />
             <Route path={"/signup"} component={Signup} />
             <Route path={"/new-org/:token"} component={NewOrg} />
             <Route path="/app" component={App}/>
