94c10bfd5028771f443527851a271d597c29f877
neal <neal2034@gmail.com>
2021-09-29 00:31:57 +0800

    update for transfer

--- src/renderer/pages/home-page/home-page.tsx
+++ src/renderer/pages/home-page/home-page.tsx
@@ -1,33 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import HomeLogo from '@imgs/elgo-logo.png'
 import Process from '@imgs/home-page/process.png'
 import Statistics from '@imgs/home-page/statistics.png'
 import TeamWork from '@imgs/home-page/team-work.png'
 import SingleCommunication from  '@imgs/home-page/single-comunication.png'
 import './home-page.less'
-import {useHistory} from "react-router";
+import {Route, useHistory} from "react-router";
+import {Switch, useRouteMatch} from "react-router-dom";
+import HomeContent from "./home-conetnt/home-content";
+import AppDownload from "./app-download/app-download";
 
 
 export default function HomePage(){
     const history = useHistory()
+    const {path} = useRouteMatch()
+    const [homeKey, setHomeKey] = useState('home')
+    const menus = [
+        {menuKey:'help', path:'/home/help', name:'帮助'},
+        {menuKey:'blog', path:'/home/blog', name:'社区'},
+        {menuKey:'download', path:'/home/download', name:'下载'},
+        {menuKey:'about', path:'/home/about', name:'关于'},
+    ]
     const response = {
         goLogin: ()=>{
             history.push("/login")
         },
         goSignup: ()=>{
             history.push("/signup")
+        },
+        menuClick: (menu:any)=>{
+            history.push(menu.path)
+            setHomeKey(menu.menuKey)
+        },
+        goHome: ()=>{
+            history.push('/')
         }
     }
+
+    useEffect(()=>{
+        const defaultPath = `${path}/content`
+        history.push(defaultPath)
+    },[])
+
+
 
     return (
         <div className="elgo-home d-flex-column">
             <div className="header d-flex justify-between align-center">
                 <div className="d-flex align-center">
-                    <img style={{height:'36px'}} src={HomeLogo}/>
+                    <img className="cursor-pointer" onClick={response.goHome} style={{height:'36px'}} src={HomeLogo}/>
                     <div className="d-flex align-center menus">
-                        <span className="menu-item">帮助</span>
-                        <span className="menu-item">社区</span>
-                        <span className="menu-item">关于我们</span>
+                        {menus.map(item=> <span onClick={()=>response.menuClick(item)} className={`menu-item ${homeKey==item.menuKey?'active':''}`}>{item.name}</span>)}
                     </div>
                 </div>
                 <div className="btn-group d-flex align-center">
@@ -36,42 +59,11 @@ export default function HomePage(){
                 </div>
             </div>
 
-            <div className="content">
-                <div style={{marginTop:'40px'}} className="d-flex justify-between align-center">
-                    <div className="d-flex-column text-part">
-                        <span className="title">高效组织软件研发流程</span>
-                        <span className="description mt40">以项目为核心，需求驱动，任务追踪，接口同步等方式，确保<br/>研发流程协调一致</span>
-                        <div onClick={response.goSignup} className="btn signup mt40">免费试用</div>
-                    </div>
-                    <img className="des-img"  src={Process}/>
-                </div>
-
-                <div style={{marginTop:'100px'}} className="d-flex justify-between align-center">
-                    <img className="des-img" src={Statistics}/>
-                    <div className="d-flex-column right-text-part">
-                        <span className="title">随时掌控研发动态</span>
-                        <span className="description mt40">对研发状态实时汇总，统计总结，随时获取当前研发状态，<br/>规避进度风险</span>
-                    </div>
-                </div>
-
-                <div style={{marginTop:'100px'}} className="d-flex justify-between align-center">
-                    <div className="d-flex-column text-part">
-                        <span className="title">需求，功能，任务，接口，<br/>测试用例，BUG相互关联，<br/>统一管理</span>
-                        <span className="description mt40">对软件项目的整个声明周期要素进行相关关联，信息同步，使得产品经理，<br/>项目经理，研发人员，测试人员，始终在同一信息层面协调工作</span>
-                    </div>
-                    <img className="des-img" src={TeamWork}/>
-                </div>
-
-                <div style={{marginTop:'100px'}} className="d-flex justify-between align-center">
-                    <img className="des-img" src={SingleCommunication}/>
-                    <div className="d-flex-column right-text-part">
-                        <span className="title">智能协同，解耦沟通</span>
-                        <span className="description mt40">更适合远程工作，多种方式同步流程，信息触达，生活工作更容易平衡</span>
-                    </div>
-                </div>
-
-                <div className="copyright justify-center align-center d-flex">Copyright ©1998-2021 Elgo All Rights Reserved   陕ICP备17005318号-2</div>
-            </div>
+            <Switch>
+                <Route path="/home/content" component={HomeContent} />
+                <Route path={'/home/download'} component={AppDownload} />
+            </Switch>
+            <div className="copyright justify-center align-center d-flex">Copyright ©1998-2021 Elgo All Rights Reserved   陕ICP备17005318号-2</div>
 
         </div>
     )
